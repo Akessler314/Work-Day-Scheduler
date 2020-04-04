77fefe06148ee801774f5819995b63a058bc9e31
Andrew Kessler <andrew.kessler95@gmail.com>
2020-04-03 21:07:42 -0600

    added all of the if statements for the CSS

--- Assets/script.js
+++ Assets/script.js
@@ -7,17 +7,99 @@ var time = day.getHours()
 $(document).ready(function() {
   var currentDayElement = $('#currentDay');
   currentDayElement.html(day.toDateString());
-  console.log(time)
+  console.log(time);
 
-  if (time === 21) {
+  var time = 10;
+  //loads the preseant css class if it is the current time 
+  if (9 <= time&& 10 > time) {
+    $("#hourNine").addClass("present")
+  } else if (9 > time) {
+    $("#hourNine").addClass("future")
+  }
+  else {
+    $("#hourNine").addClass("past")
+  }
+
+  if (10 <= time&& 11 > time) {
+    $("#hourTen").addClass("present")
+  } else if (10 > time) {
     $("#hourTen").addClass("future")
   }
+  else {
+    $("#hourTen").addClass("past")
+  }
+
+  if (11 <= time&& 12 > time) {
+    $("#hourEleven").addClass("present")
+  } else if (11 > time) {
+    $("#hourEleven").addClass("future")
+  }
+  else {
+    $("#hourEleven").addClass("past")
+  }
+  
+  if (12 <= time&& 13 > time) {
+    $("#hourTwelve").addClass("present")
+  } else if (12 > time) {
+    $("#hourTwelve").addClass("future")
+  }
+  else {
+    $("#hourTwelve").addClass("past")
+  }
+
+  if (13 <= time&& 14 > time) {
+    $("#hourOne").addClass("present")
+  } else if (13 > time) {
+    $("#hourOne").addClass("future")
+  }
+  else {
+    $("#hourOne").addClass("past")
+  }
+
+  if (14 <= time&& 15 > time) {
+    $("#hourTwo").addClass("present")
+  } else if (14 > time) {
+    $("#hourTwo").addClass("future")
+  }
+  else {
+    $("#hourTwo").addClass("past")
+  }
+
+  if (15 <= time&& 16 > time) {
+    $("#hourThree").addClass("present")
+  } else if (15 > time) {
+    $("#hourThree").addClass("future")
+  }
+  else {
+    $("#hourThree").addClass("past")
+  }
+
+  if (16 <= time&& 17 > time) {
+    $("#hourFour").addClass("present")
+  } else if (16 > time) {
+    $("#hourFour").addClass("future")
+  }
+  else {
+    $("#hourFour").addClass("past")
+  }
+
+  if (17 <= time&& 18 > time) {
+    $("#hourFive").addClass("present")
+  } else if (17 > time) {
+    $("#hourFive").addClass("future")
+  }
+  else {
+    $("#hourFive").addClass("past")
+  }
+
+
   });
   
 
 function addHighlight() {
+ 
   //function that will be used to highlight the Divs with past presant and future 
-}
+};
 
 
   //WHAT NEEDS TO HAPPED 
